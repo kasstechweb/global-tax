ed740ee90f0bdaefa5cf9b92dd143c7c85a11d9e
kasstechweb <kasstechweb@gmail.com>
2022-11-11 18:00:53 +0200

    welcome page finished

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ const routes: Routes = [
   //   // loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   // },
   {
-    path: 'welcome',
+    path: '',
     loadChildren: () => import('./welcome/welcome.module').then( m => m.WelcomePageModule)
   }
 ];
