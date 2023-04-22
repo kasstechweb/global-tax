a114416075187a2a77a2bcfa7e0604616b954444
kasstechweb <kasstechweb@gmail.com>
2023-04-22 17:25:40 +0200

    ocr working from scanned image

--- src/app/scan/scan.page.ts
+++ src/app/scan/scan.page.ts
@@ -6,6 +6,11 @@ import {NavController} from "@ionic/angular";
 import {IonLoaderService} from "../services/ion-loader.service";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 
+//reading image import
+import {createWorker, PSM} from 'tesseract.js'
+import * as Tesseract from "tesseract.js";
+import {Validators} from "@angular/forms";
+
 @Component({
   selector: 'app-scan',
   templateUrl: './scan.page.html',
@@ -14,12 +19,26 @@ import {HttpClient, HttpHeaders} from "@angular/common/http";
 export class ScanPage implements OnInit {
   // friends: Array<any>;
   site_url = 'https://localhost/gtax_receipt_scanner';
-  icon_url = 'https://app.outboundsales.io/api/logo/';
+  // icon_url = 'https://app.outboundsales.io/api/logo/';
+  icon_url = 'https://logo.clearbit.com/'
   icon_name: string;
   friendsList: Array<any>;
   friends: Array<any>;
   searchQuery = '';
   user_id: string;
+
+  //reading image stuff
+  worker: Tesseract.Worker;
+  workerReady = false;
+  image = 'https://tesseract.projectnaptha.com/img/eng_bw.png'
+  ocrResult = '';
+  captureProgress = 0;
+  show_progress = false;
+  result_array: Array<any>;
+  subtotal_amount: string;
+  gst_amount: string;
+  total_amount: string;
+
   // test = 'test'
 //   friendsList: ;
 
@@ -54,6 +73,7 @@ export class ScanPage implements OnInit {
     public http: HttpClient,
   ) {
     this.ionLoader.showLoader().then(()=> {
+      this.loadWorker();
       this.storage.getStorageData('scan_logged_in').then(
         res => {
           console.log(res);
@@ -99,17 +119,17 @@ export class ScanPage implements OnInit {
 
   }
 
-  async captureImage() {
-      const image = await Camera.getPhoto({
-        quality: 90,
-        allowEditing: true,
-        resultType: CameraResultType.DataUrl,
-        source: CameraSource.Camera
-      });
-      console.log('image: ', image);
-      // this.image = image.dataUrl;
-
-    }
+  // async captureImage() {
+  //     const image = await Camera.getPhoto({
+  //       quality: 90,
+  //       allowEditing: true,
+  //       resultType: CameraResultType.DataUrl,
+  //       source: CameraSource.Camera
+  //     });
+  //     console.log('image: ', image);
+  //     // this.image = image.dataUrl;
+  //
+  //   }
 
   searchList(): void {
     const query = (this.searchQuery && this.searchQuery !== null) ? this.searchQuery : '';
@@ -130,9 +150,12 @@ export class ScanPage implements OnInit {
           result => {
             console.log('result: ' + result)
           }, (err) => {
+          // console.log(err)
             if(err.status == '200'){
               list[i].icon = full_icon_url
             }else if(err.status == '501'){ // error couldn't get image from url
+              list[i].icon = this.icon_url + small_name + '.com'
+            }else if(err.status == '0'){ // error couldn't get image from url
               list[i].icon = this.icon_url + small_name + '.com'
             }
           });
@@ -140,4 +163,113 @@ export class ScanPage implements OnInit {
     this.friends = list
     console.log(this.friends)
   }
+
+  async loadWorker() {
+    this.worker = await createWorker({
+      logger: progress => {
+        console.log(progress);
+        if (progress.status == 'recognizing text') {
+          // this.captureProgress = parseInt('' + progress.progress * 100);
+          this.captureProgress = progress.progress
+        }
+      }
+    });
+    await this.worker.load();
+    await this.worker.loadLanguage('fin');
+    await this.worker.initialize('fin');
+    await this.worker.setParameters({
+      tessedit_pageseg_mode: PSM.SPARSE_TEXT,
+    });
+
+    console.log('Finish')
+    this.workerReady = true;
+  }
+
+  async captureImage() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.DataUrl,
+      source: CameraSource.Camera
+    });
+    console.log('image: ', image);
+    this.image = image.dataUrl;
+    this.recognizeImage();
+  }
+
+  async recognizeImage() {
+
+    this.ionLoader.showLoader().then(()=>{
+      this.show_progress = true;
+    });
+    this.image = './assets/imgs/0001.jpg' // debug remove later
+    // this.image = './assets/imgs/0002.jpg' // debug remove later
+    // this.image = './assets/imgs/0003.jpeg' // debug remove later
+    // this.image = './assets/imgs/0005.jpg' // debug remove later
+    // this.image = './assets/imgs/0009.jpg' // debug remove later
+    // this.image = './assets/imgs/0007.jpg' // debug remove later
+    // this.image = 'https://tesseract.projectnaptha.com/img/eng_bw.png'
+    const result = await this.worker.recognize(this.image);
+
+    const result_logo = await this.worker.recognize(this.image,
+      {rectangle: { top: 0, left: 0, width: 1000, height: 500 }
+      });
+
+    console.log(result);
+    
+    this.result_array = result.data.lines
+    for (let item of this.result_array) { // loop through the result and remove text that has confidence < 60
+      if (item.confidence > 60) {
+        console.log(item.text)
+        this.ocrResult += item.text
+      }
+    }
+
+    //get subtotal
+    // the pattern
+    const regExp = new RegExp('(\\bSubtotal:?\\n\\n\\b)(\\d{1,3}(?:,?\\d{3})*\\.\\d{2})')
+    const regExpGST = new RegExp('(\\bGST\\b[\\/]?(\\w.*)?\\n\\n)(\\d{1,3}(?:,?\\d{3})*\\.\\d{2})');
+    // pattern for small receipt
+    const regExpTotal = new RegExp('(\\bTotal\\n\\n\\$?\\b)(\\d{1,3}(?:,?\\d{3})*\\.\\d{2})');
+
+    if(this.ocrResult.match(regExp)){ // if subtotal word found
+      this.subtotal_amount = this.ocrResult.match(regExp)[2];
+      if(this.ocrResult.match(regExpGST)){ // check for gst word
+        this.gst_amount = this.ocrResult.match(regExpGST)[3]
+      }
+      this.total_amount = (parseFloat(this.subtotal_amount) + parseFloat(this.gst_amount)).toString();
+    }else if(this.ocrResult.match(regExpTotal)){ // if total found means small receipt
+      console.log(this.ocrResult.match(regExpTotal)[2]);
+      this.total_amount = this.ocrResult.match(regExpTotal)[2];
+      // get subtotal from total calculations
+      let total_float = parseFloat(this.total_amount);
+      let subtotal_float = (total_float) - ((total_float / (1 + 0.05)) * 0.05);
+      let gst_float = Math.round(total_float - subtotal_float);
+      this.subtotal_amount = subtotal_float.toFixed(2).toString();
+      this.gst_amount = gst_float.toFixed(2).toString();
+    }
+
+    // get logo text
+    let logo_text = result_logo.data.lines[0].text;
+    if (logo_text.includes('TRANSACTION RECORD')) { // if this word on logo skip to next line
+      let index = 1;
+      while(result_logo.data.lines[index].confidence < 60){
+        index++;
+      }
+      logo_text = result_logo.data.lines[index].text;
+    }
+    logo_text = logo_text.replace('\n', '');
+
+    // log the results
+    console.log('LOGO ==> ' + logo_text)
+    console.log('Subtotal ==> ' + this.subtotal_amount)
+    console.log('GST ==> ' + this.gst_amount)
+    console.log('Total ==> ' + this.total_amount)
+
+
+    this.show_progress = false;
+    this.ionLoader.dismissLoader();
+    await this.worker.terminate();
+  }
+
 }
